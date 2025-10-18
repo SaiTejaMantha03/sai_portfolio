769e575ead28728df13b5325451566105fdeb89c
Sai Teja Mantha <saitejamantha@Sais-MacBook-Air.local>
2025-10-18 12:17:21 +0530

    Remove MySQL for faster deployment

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -63,19 +63,7 @@ DATABASES = {
     }
 }
 
-# Use MySQL if environment variables are provided
-if os.environ.get('DB_HOST'):
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
+# SQLite only for fast deployment
 
 AUTH_PASSWORD_VALIDATORS = [
     {
