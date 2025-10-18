137c292169c830eb437baa397059f7417c06d39c
Sai Teja Mantha <saitejamantha@Sais-MacBook-Air.local>
2025-10-18 12:07:07 +0530

    Use SQLite by default, MySQL only with env vars

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -58,17 +58,24 @@ import os
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Use MySQL if environment variables are provided
+if os.environ.get('DB_HOST'):
+    DATABASES['default'] = {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME', 'portfolio_sai'),
-        'USER': os.environ.get('DB_USER', 'admin'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', '34.41.247.198'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT', '3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
